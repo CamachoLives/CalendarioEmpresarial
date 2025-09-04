76f04dc8b2133804eb42b25faaa871f997312e45
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-04 16:02:31 -0500

    Agregando los errores

--- CalendarioBackEnd/src/Activities/controller.js
+++ CalendarioBackEnd/src/Activities/controller.js
@@ -1,32 +1,39 @@
 const debug = require("debug")("app:activities-controller");
 const { activitiesService } = require("./services");
+const { response } = require("../common/response");
+const createErrors = require("http-errors");
 
 module.exports.activitiesController = {
   getactivities: async (req, res) => {
     try {
+      console.log("Entre");
       let activities = await activitiesService.Getall();
-      res.json(activities);
+      response.success(res, "We will get the activities!", 200, activities);
     } catch (error) {
       debug(error);
-      res.status(500).json({ message: error.message });
+      response.error(res);
     }
   },
   getactivityById: async (req, res) => {
     try {
       let id = await activitiesService.getById(req.id);
-      res.status(201).json(id);
+      if (!id) {
+        response.error(res, new createErrors.NotFound());
+      } else {
+        response.success(res, "Activities by ID!", 200, id);
+      }
     } catch (error) {
       debug(error);
-      res.status(500).json({ message: error.message });
+      response.error(res);
     }
   },
   createactivity: async (req, res) => {
     try {
-      let id = await activitiesService.create(req.body);
-      res.status(201).json(id);
+      let Create = await activitiesService.create(req.body);
+      response.success(res, "Create Activitie!", 200, req.body);
     } catch (error) {
       debug(error);
-      res.status(404).json({ message: error.message });
+      response.error(res);
     }
   },
 };

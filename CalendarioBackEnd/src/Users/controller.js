fd966430d325392d049b4474ff3cc92f9b81e988
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-08 22:05:39 -0500

    i have fixed the issue that i had with get information

--- CalendarioBackEnd/src/Users/controller.js
+++ CalendarioBackEnd/src/Users/controller.js
@@ -5,7 +5,6 @@ const { response } = require("../common/response");
 module.exports.usersController = {
   getEveryone: async (req, res) => {
     try {
-      console.log(req.query.email);
       let user = await UsersService.Getall(req.query.email);
       response.success(res, "We will get the person's everything!", 200, user);
     } catch (error) {
--- CalendarioBackEnd/src/Users/services.js
+++ CalendarioBackEnd/src/Users/services.js
@@ -3,7 +3,6 @@ const { database } = require("../database/index");
 const { UsersRepository } = require("./repository");
 
 const Getall = async (Email) => {
-  console.log("Email --> ", Email);
   const all = await UsersRepository.getEverything(Email);
   return all;
 };
